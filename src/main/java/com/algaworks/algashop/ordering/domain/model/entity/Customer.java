e37f278d3d12b97931be668be5c90994334d3541
Alexandre Henrique Unfried <alexandreunfried@gmail.com>
2025-10-09 21:28:36 -0300

    Código até a aula 7.5. Utilizando a Layered Architecture na prática do curso Especialista Microsserviços - Nível 2

---
+++ src/main/java/com/algaworks/algashop/ordering/domain/model/entity/Customer.java
@@ -0,0 +1,248 @@
+package com.algaworks.algashop.ordering.domain.model.entity;
+
+import com.algaworks.algashop.ordering.domain.model.exception.CustomerArchivedException;
+import com.algaworks.algashop.ordering.domain.model.valueobject.*;
+import com.algaworks.algashop.ordering.domain.model.valueobject.id.CustomerId;
+import lombok.Builder;
+
+import java.time.OffsetDateTime;
+import java.util.Objects;
+import java.util.UUID;
+
+import static com.algaworks.algashop.ordering.domain.model.exception.ErrorMessages.VALIDATION_ERROR_FULLNAME_IS_NULL;
+
+public class Customer {
+
+	private CustomerId id;
+	private FullName fullName;
+	private BirthDate birthDate;
+	private Email email;
+	private Phone phone;
+	private Document document;
+	private Boolean promotionNotificationsAllowed;
+	private Boolean archived;
+	private OffsetDateTime registeredAt;
+	private OffsetDateTime archivedAt;
+	private LoyaltyPoints loyaltyPoints;
+	private Address address;
+
+	@Builder(builderClassName = "BrandNewCustomerBuild", builderMethodName = "brandNew")
+	private static Customer createBrandNew(
+			FullName fullName,
+			BirthDate birthDate,
+			Email email,
+			Phone phone,
+			Document document,
+			Boolean promotionNotificationsAllowed,
+			Address address
+	) {
+		return new Customer(
+				new CustomerId(),
+				fullName,
+				birthDate,
+				email,
+				phone,
+				document,
+				promotionNotificationsAllowed,
+				false,
+				OffsetDateTime.now(),
+				null,
+				LoyaltyPoints.ZERO,
+				address
+		);
+	}
+
+	@Builder(builderClassName = "ExistingCustomerBuild", builderMethodName = "existing")
+	private Customer(CustomerId id, FullName fullName, BirthDate birthDate, Email email, Phone phone,
+					 Document document, Boolean promotionNotificationsAllowed, Boolean archived,
+					 OffsetDateTime registeredAt, OffsetDateTime archivedAt, LoyaltyPoints loyaltyPoints, Address address) {
+		setId(id);
+		setFullName(fullName);
+		setBirthDate(birthDate);
+		setEmail(email);
+		setPhone(phone);
+		setDocument(document);
+		setPromotionNotificationsAllowed(promotionNotificationsAllowed);
+		setArchived(archived);
+		setRegisteredAt(registeredAt);
+		setArchivedAt(archivedAt);
+		setLoyaltyPoints(loyaltyPoints);
+		setAddress(address);
+	}
+
+	public void addLoyaltyPoints(LoyaltyPoints loyaltyPointsAdded) {
+		verifyIfChangeable();
+		setLoyaltyPoints(loyaltyPoints().add(loyaltyPointsAdded));
+	}
+
+	public void archive() {
+		verifyIfChangeable();
+		setArchived(true);
+		setArchivedAt(OffsetDateTime.now());
+		setFullName(new FullName("Anonymous", "Anonymous"));
+		setPhone(new Phone("000-000-0000"));
+		setDocument(new Document("000-00-0000"));
+		setEmail(new Email(UUID.randomUUID() + "@anonymous.com"));
+		setBirthDate(null);
+		setPromotionNotificationsAllowed(false);
+		setAddress(address().toBuilder().number("Anonymized").complement(null).build());
+	}
+
+	public void enablePromotionNotifications() {
+		verifyIfChangeable();
+		setPromotionNotificationsAllowed(true);
+	}
+
+	public void disablePromotionNotifications() {
+		verifyIfChangeable();
+		setPromotionNotificationsAllowed(false);
+	}
+
+	public void changeName(FullName fullName) {
+		verifyIfChangeable();
+		setFullName(fullName);
+	}
+
+	public void changeEmail(Email email) {
+		verifyIfChangeable();
+		setEmail(email);
+	}
+
+	public void changePhone(Phone phone) {
+		verifyIfChangeable();
+		setPhone(phone);
+	}
+
+	public void changeAddress(Address address) {
+		verifyIfChangeable();
+		setAddress(address);
+	}
+
+	public CustomerId id() {
+		return id;
+	}
+
+	public FullName fullName() {
+		return fullName;
+	}
+
+	public BirthDate birthDate() {
+		return birthDate;
+	}
+
+	public Email email() {
+		return email;
+	}
+
+	public Phone phone() {
+		return phone;
+	}
+
+	public Document document() {
+		return document;
+	}
+
+	public Boolean isPromotionNotificationsAllowed() {
+		return promotionNotificationsAllowed;
+	}
+
+	public Boolean isArchived() {
+		return archived;
+	}
+
+	public OffsetDateTime registeredAt() {
+		return registeredAt;
+	}
+
+	public OffsetDateTime archivedAt() {
+		return archivedAt;
+	}
+
+	public LoyaltyPoints loyaltyPoints() {
+		return loyaltyPoints;
+	}
+
+	public Address address() {
+		return address;
+	}
+
+	private void setId(CustomerId id) {
+		Objects.requireNonNull(id);
+		this.id = id;
+	}
+
+	private void setFullName(FullName fullName) {
+		Objects.requireNonNull(fullName, VALIDATION_ERROR_FULLNAME_IS_NULL);
+		this.fullName = fullName;
+	}
+
+	private void setBirthDate(BirthDate birthDate) {
+		if (birthDate == null) {
+			this.birthDate = null;
+			return;
+		}
+		this.birthDate = birthDate;
+	}
+
+	private void setEmail(Email email) {
+		Objects.requireNonNull(email);
+		this.email = email;
+	}
+
+	private void setPhone(Phone phone) {
+		Objects.requireNonNull(phone);
+		this.phone = phone;
+	}
+
+	private void setDocument(Document document) {
+		Objects.requireNonNull(document);
+		this.document = document;
+	}
+
+	private void setPromotionNotificationsAllowed(Boolean promotionNotificationsAllowed) {
+		Objects.requireNonNull(promotionNotificationsAllowed);
+		this.promotionNotificationsAllowed = promotionNotificationsAllowed;
+	}
+
+	private void setArchived(Boolean archived) {
+		Objects.requireNonNull(archived);
+		this.archived = archived;
+	}
+
+	private void setRegisteredAt(OffsetDateTime registeredAt) {
+		Objects.requireNonNull(registeredAt);
+		this.registeredAt = registeredAt;
+	}
+
+	private void setArchivedAt(OffsetDateTime archivedAt) {
+		this.archivedAt = archivedAt;
+	}
+
+	private void setLoyaltyPoints(LoyaltyPoints loyaltyPoints) {
+		Objects.requireNonNull(loyaltyPoints);
+		this.loyaltyPoints = loyaltyPoints;
+	}
+
+	private void setAddress(Address address) {
+		Objects.requireNonNull(address);
+		this.address = address;
+	}
+
+	private void verifyIfChangeable() {
+		if (this.isArchived()) {
+			throw new CustomerArchivedException();
+		}
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (o == null || getClass() != o.getClass()) return false;
+		Customer customer = (Customer) o;
+		return Objects.equals(id, customer.id);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hashCode(id);
+	}
+}
---
+++ src/test/java/com/algaworks/algashop/ordering/domain/model/entity/OrderItemTest.java
@@ -0,0 +1,35 @@
+package com.algaworks.algashop.ordering.domain.model.entity;
+
+import com.algaworks.algashop.ordering.domain.model.valueobject.Product;
+import com.algaworks.algashop.ordering.domain.model.valueobject.Quantity;
+import com.algaworks.algashop.ordering.domain.model.valueobject.id.OrderId;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static com.algaworks.algashop.ordering.domain.model.entity.ProductTestDataBuilder.aProductAltMousePad;
+
+class OrderItemTest {
+
+	@Test
+	void shouldGenerateBrandNewOrderItem() {
+		Product product = aProductAltMousePad().build();
+		Quantity quantity = new Quantity(1);
+		OrderId orderId = new OrderId();
+
+		OrderItem orderItem = OrderItem.brandNew()
+				.product(product)
+				.quantity(quantity)
+				.orderId(orderId)
+				.build();
+
+		Assertions.assertWith(orderItem,
+				i -> Assertions.assertThat(i.id()).isNotNull(),
+				i -> Assertions.assertThat(i.productId()).isEqualTo(product.id()),
+				i -> Assertions.assertThat(i.productName()).isEqualTo(product.name()),
+				i -> Assertions.assertThat(i.price()).isEqualTo(product.price()),
+				i -> Assertions.assertThat(i.quantity()).isEqualTo(quantity),
+				i -> Assertions.assertThat(i.orderId()).isEqualTo(orderId)
+		);
+	}
+
+}
