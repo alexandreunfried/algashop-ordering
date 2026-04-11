20f9cc24d46d26f1c8fcebfc339686ac582a32a6
Alexandre Henrique Unfried <alexandreunfried@gmail.com>
2026-04-11 10:51:13 -0300

    Código até a aula 13.7. Garantindo escuta de eventos com Mockito Spy do Curso Especialista Microsserviços - Nível 2

--- src/test/java/com/algaworks/algashop/ordering/application/customer/management/CustomerManagementApplicationServiceIT.java
+++ src/test/java/com/algaworks/algashop/ordering/application/customer/management/CustomerManagementApplicationServiceIT.java
@@ -1,9 +1,14 @@
 package com.algaworks.algashop.ordering.application.customer.management;
 
+import com.algaworks.algashop.ordering.domain.model.customer.CustomerArchivedEvent;
+import com.algaworks.algashop.ordering.domain.model.customer.CustomerRegisteredEvent;
+import com.algaworks.algashop.ordering.infrastructure.listener.customer.CustomerEventListener;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoSpyBean;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
@@ -15,6 +20,9 @@ class CustomerManagementApplicationServiceIT {
 
 	@Autowired
 	private CustomerManagementApplicationService customerManagementApplicationService;
+
+	@MockitoSpyBean
+	private CustomerEventListener customerEventListener;
 
 	@Test
 	void shouldRegister() {
@@ -42,6 +50,15 @@ class CustomerManagementApplicationServiceIT {
 		Assertions.assertThat(customerOutput.getAddress().getCity()).isEqualTo("Yostfort");
 		Assertions.assertThat(customerOutput.getAddress().getState()).isEqualTo("South Carolina");
 		Assertions.assertThat(customerOutput.getAddress().getZipCode()).isEqualTo("70283");
+
+		Mockito.verify(customerEventListener)
+				.listen(Mockito.any(CustomerRegisteredEvent.class));
+
+		Mockito.verify(customerEventListener)
+				.listenSecondary(Mockito.any(CustomerRegisteredEvent.class));
+
+		Mockito.verify(customerEventListener, Mockito.never())
+				.listen(Mockito.any(CustomerArchivedEvent.class));
 	}
 
 	@Test
