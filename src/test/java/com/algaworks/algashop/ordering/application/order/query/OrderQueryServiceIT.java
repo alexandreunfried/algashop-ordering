4977ce99493bb8fa7963594abb9fda03ee3ac6b2
Alexandre Henrique Unfried <alexandreunfried@gmail.com>
2026-05-06 21:53:28 -0300

    Código até a aula 15.7. Consulta paginada para Order com Criteria API do Curso Especialista Microsserviços - Nível 2

---
+++ src/test/java/com/algaworks/algashop/ordering/application/order/query/OrderQueryServiceIT.java
@@ -0,0 +1,100 @@
+package com.algaworks.algashop.ordering.application.order.query;
+
+import com.algaworks.algashop.ordering.application.utility.PageFilter;
+import com.algaworks.algashop.ordering.domain.model.customer.Customer;
+import com.algaworks.algashop.ordering.domain.model.customer.CustomerTestDataBuilder;
+import com.algaworks.algashop.ordering.domain.model.customer.Customers;
+import com.algaworks.algashop.ordering.domain.model.order.Order;
+import com.algaworks.algashop.ordering.domain.model.order.OrderStatus;
+import com.algaworks.algashop.ordering.domain.model.order.OrderTestDataBuilder;
+import com.algaworks.algashop.ordering.domain.model.order.Orders;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+class OrderQueryServiceIT {
+
+	@Autowired
+	private OrderQueryService queryService;
+
+	@Autowired
+	private Orders orders;
+
+	@Autowired
+	private Customers customers;
+
+	@Test
+	void shouldFindById() {
+		Customer customer = CustomerTestDataBuilder.existingCustomer().build();
+		customers.add(customer);
+
+		Order order = OrderTestDataBuilder.anOrder().customerId(customer.id()).build();
+		orders.add(order);
+
+		OrderDetailOutput output = queryService.findById(order.id().toString());
+
+		Assertions.assertThat(output)
+				.extracting(
+						OrderDetailOutput::getId,
+						OrderDetailOutput::getTotalAmount
+				)
+				.containsExactly(
+						order.id().toString(),
+						order.totalAmount().value()
+				);
+	}
+
+	@Test
+	void shouldFilterByPage() {
+		Customer customer = CustomerTestDataBuilder.existingCustomer().build();
+		customers.add(customer);
+
+		Order order1 = OrderTestDataBuilder.anOrder()
+				.status(OrderStatus.DRAFT)
+				.withItems(false)
+				.customerId(customer.id())
+				.build();
+
+		orders.add(order1);
+
+		Order order2 = OrderTestDataBuilder.anOrder()
+				.status(OrderStatus.PLACED)
+				.customerId(customer.id())
+				.build();
+
+		orders.add(order2);
+
+		Order order3 = OrderTestDataBuilder.anOrder()
+				.status(OrderStatus.PAID)
+				.customerId(customer.id())
+				.build();
+
+		orders.add(order3);
+
+		Order order4 = OrderTestDataBuilder.anOrder()
+				.status(OrderStatus.READY)
+				.customerId(customer.id())
+				.build();
+
+		orders.add(order4);
+
+		Order order5 = OrderTestDataBuilder.anOrder()
+				.status(OrderStatus.CANCELED)
+				.customerId(customer.id())
+				.build();
+
+		orders.add(order5);
+
+		Page<OrderSummaryOutput> page = queryService.filter(new PageFilter(3, 0));
+
+		Assertions.assertThat(page.getTotalPages()).isEqualTo(2);
+		Assertions.assertThat(page.getTotalElements()).isEqualTo(5);
+		Assertions.assertThat(page.getNumberOfElements()).isEqualTo(3);
+	}
+
+}
